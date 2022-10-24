b61d4461b90b38b44be77748df17810b0239e6af
rk <brenotm10@gmail.com>
2022-10-24 08:47:39 -0300

    Create tipagem-dinamica.js

---
+++ fundamentos/tipagem-dinamica.js
@@ -0,0 +1,13 @@
+var idade = 25;
+console.log(typeof idade);
+console.log(idade);
+
+idade += ' anos';
+
+console.log(typeof idade);
+console.log(idade);
+
+idade = false;
+console.log(typeof idade);
+console.log(idade);
+
