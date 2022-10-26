0256058c1e4e00fada84971ee6703778b0e32b3d
rk <brenotm10@gmail.com>
2022-10-26 20:32:52 -0300

    Create tipagem-boolean.js

---
+++ fundamentos/tipagem-boolean.js
@@ -0,0 +1,13 @@
+// 0 é false
+
+if (0) {
+     console.log('0 é true');
+}   else {
+     console.log('0 é false');
+}
+
+// string vazia
+//NaN é false
+//null é false
+//underfined é false
+//false é false 
