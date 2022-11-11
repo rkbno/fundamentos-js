273db1a9f4f7fb62571d564dee06a91e19079c18
rk <brenotm10@gmail.com>
2022-11-11 11:16:07 -0300

    estudos js

---
+++ arrays/criando-arrays.js
@@ -0,0 +1,13 @@
+var emails = [];
+emails[0] = 'joao@email.com';
+emails[1] = 'maria@email.com';
+
+console.log('emails[0]', emails[0]);
+console.log('emails[5]');
+
+var nomes = ['João', 'Maria'];
+console.log('quantos nomes?', nomes.length);
+
+nomes [4] = 'José';
+
+console.log('quantos nomes depois do José', nomes[3]);
---
+++ arrays/introducao-arrays.js
@@ -0,0 +1,2 @@
+// no js vc consegue adicionar sem limites
+// no java tem limite
