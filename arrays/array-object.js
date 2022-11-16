c442431618eb3051ba30aedad8af2905a5b0816e
rk <brenotm10@gmail.com>
2022-11-16 12:08:22 -0300

    Create array-object.js

---
+++ arrays/array-object.js
@@ -0,0 +1,17 @@
+var foxArray = ['Fox', 'Prata', 'VW'];
+typeof foxArray
+
+var foxObj = { modelo: 'Fox', cor: 'Prata' }
+typeof foxObj
+
+// a diferença do array pro objeto é que o array para escolher precisa ser por numeração, ja o objeto por nome, ex:
+
+foxArray[0]
+// Fox
+foxArray[1]
+//Prata
+
+foxObj.modelo
+// Fox
+foxObj.cor
+// Prata
